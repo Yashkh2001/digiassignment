2d09943554a036c2c4ceb60d49e9d4efebd79239
Yash Khatwani <yash.kh2001@gmail.com>
2022-12-05 01:17:46 +0530

    Updates

---
+++ precache-manifest.5bf31c6e8ad80d69a4d2ba729ed880a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84c31f9959ebafd7a2fae1e6f15ffef",
+    "url": "/digiassignment/index.html"
+  },
+  {
+    "revision": "dd22e7c54b40c3d86c0c",
+    "url": "/digiassignment/static/css/main.720b038b.chunk.css"
+  },
+  {
+    "revision": "78977ff360a6b5a21af8",
+    "url": "/digiassignment/static/js/2.da697d0d.chunk.js"
+  },
+  {
+    "revision": "dd22e7c54b40c3d86c0c",
+    "url": "/digiassignment/static/js/main.fd32f198.chunk.js"
+  },
+  {
+    "revision": "1f45e6f4daf71fd26719",
+    "url": "/digiassignment/static/js/runtime~main.0c801397.js"
+  }
+]);
